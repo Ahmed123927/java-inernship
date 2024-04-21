2b491f8f241843c65f572a6e9826bcff331e8818
Ahmed123927 <ah7070652@gmail.com>
2024-04-21 02:51:45 +0200

    Week 3 Tasks

--- week1/hellworld.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class hellworld {
-    public static void main(String[] args) {
-               System.out.println("hello ahmed ");
-
-    }
-}
